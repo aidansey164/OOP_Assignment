156dc657e640b645e3cef78be4304326542def32
aidansey164 <46528161+aidansey164@users.noreply.github.com>
2022-01-18 14:54:13 +0100

    add main class, cmakes, update graph with new functions

--- Task1/Graph.cpp
+++ Task1/Graph.cpp
@@ -1,20 +1,50 @@
+#include "Graph.hpp"
 using namespace std;
 
 template<typename T>
 class Graph {
 public:
     vector<vector<Vertex<T> > > adjacList;
-    vector<Vertex<T> > src_nodes;
     vector<Vertex<T> > path_nodes;
+    vector<Vertex<T> > src_nodes;
 
-    // param constructor for graph that takes a list of edges and the number of nodes in the DAG
-    Graph<T>(vector<Edge<T> > const &edges, int no_of_nodes) {
+    // graph constructor takes list of edges and the number of nodes in DAG
+    Graph<T>(vector<Edge<T> > const &edges, int nonodes) {
+        adjacList.resize(nonodes);
+        src_nodes.resize(nonodes/2);
+        path_nodes.resize(nonodes/2);
+        for (auto &edge: edges) {
+            int i;;
+            adjacList[edge.src.nodename].push_back(edge.path.nodename);
 
+            src_nodes[i] = edge.src.nodename;
+            path_nodes[i] = edge.path.nodename;
+            i++;
+        }
     }
 
-    Graph<T>() {  // default constructor
+
+    //default constructor of creation of graph
+    Graph<T>() {
 
     }
 };
 
+// returning edges
+template<typename T>
+void returnEdges(Graph<T> const &graph, int n) {
+    for (T i = 0; i < n; i++) {
+        cout << "Vertex \t" << i << " --> ";
+
+        for (Vertex<T> v: graph.adjacList[i]) {
+            cout << v.nodename << "\t";
+        }
+        cout << endl;
+    }
+}
+
+//destroy edges
+void clearEdges(vector<Edge<int> > list){
+    list.clear();
+}
 
---
+++ Task1/main.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+#include "Graph.hpp"
+#include "Graph.cpp"
+
+int main() {
+
+
+    Vertex<int> *vertex1 = new Vertex<int>(0);
+    Vertex<int> *vertex2 = new Vertex<int>(1);
+    Vertex<int> *vertex3 = new Vertex<int>(2);
+
+
+    Edge<int> *e1 = new Edge<int>(*vertex1, *vertex2);
+    Edge<int> *e2 = new Edge<int>(*vertex2, *vertex3);
+    Edge<int> *e3 = new Edge<int>(*vertex3, *vertex1);
+
+    vector<Edge<int> > edgeList;
+    edgeList.push_back(*e1);
+    edgeList.push_back(*e2);
+    edgeList.push_back(*e3);
+    int n = 3;
+    Graph<int> g(edgeList, n);
+
+    returnEdges(g, n);
+
+
+    return 0;
+}
