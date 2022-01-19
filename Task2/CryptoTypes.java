09ed9830c4120a9fd9ee1628ff584c03d1a97b13
aidansey164 <46528161+aidansey164@users.noreply.github.com>
2022-01-19 14:15:58 +0100

    added  crypto objects

--- Task2/CryptoTypes.java
+++ Task2/CryptoTypes.java
@@ -1,4 +1,11 @@
-
 public class CryptoTypes {
+    public double price;
+    public String symbol;
+    public int stock;
 
+    public CryptoTypes(String symbol, double price, int supply) { // Constructor. Will be used to create various coins if needed.
+        this.symbol = symbol;
+        this.price = price;
+        this.stock = stock;
+    }
 }
--- Task2/main_launcher.java
+++ Task2/main_launcher.java
@@ -3,6 +3,10 @@ public class main_launcher {
 
 	public static void main(String[] args) {
 		System.out.println("Main class");
+		CryptoTypes btc = new CryptoTypes("btc", 10000, 1000); //symbol, price, stock
+		CryptoTypes doge = new CryptoTypes("doge", 10000, 1000);
+		System.out.println(btc.symbol);
+		System.out.println(doge.symbol);
 
 	}
 
