a2fbee6ed86c0d7a1ba16c6df83ceef6ee7e286f
aidansey164 <46528161+aidansey164@users.noreply.github.com>
2022-01-20 14:50:39 +0100

    user object

--- Task2/User.java
+++ Task2/User.java
@@ -1,4 +1,15 @@
 
 public class User {
+    public  int userid;
+    public int age;public String name;
+    public double FIAT_balance;
+    public double Crypto_balance = 0;
+    public boolean admin_status = false;
 
+    public User(String name, int age, double FIAT_balance, int userid) {
+        this.name = name;
+        this.age = age;
+        this.FIAT_balance = FIAT_balance;
+        this.userid = userid;
+    }
 }
