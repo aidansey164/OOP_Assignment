fb38c28249c26b039bc3092443414522e1a13ea8
aidansey164 <46528161+aidansey164@users.noreply.github.com>
2022-01-20 22:17:17 +0100

    final optimization

--- Task3/myuint.cpp
+++ Task3/myuint.cpp
@@ -301,16 +301,3 @@ type myuint<T>::convert_to()
     return result;
 }
 
-int main() {
-    myuint<1024> a(5);
-    std::cout << a.getNumber() << std::endl;
-    std::cout << a.convert_to<int>() << std::endl;
-
-    myuint<1024> b(10);
-    std::cout << b.getNumber() << std::endl;
-    std::cout << b.convert_to<int>() << std::endl;
-
-    std::cout << ((a * b) / 10).convert_to<int>() << std::endl;
-    return 0;
-
-}
