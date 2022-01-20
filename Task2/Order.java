5281be2d4722c5b9363f0dfe1c359f6eb0da56e0
aidansey164 <46528161+aidansey164@users.noreply.github.com>
2022-01-20 20:23:35 +0100

    order final

--- Task2/Order.java
+++ Task2/Order.java
@@ -1,4 +1,37 @@
-
 public class Order {
+	
+    public enum OrderType { 
+        BUY, SELL
+    }
 
+    public enum OrderTypeS { 
+        MARKET, LIMIT
+    }
+
+    public final OrderType orderType;
+    public final OrderTypeS orderSubType;
+    public double price;
+    public double quantity;
+    public final User user;
+    public final CryptoTypes crypto;
+    public boolean isFilled = false;
+
+
+
+    public Order(User user, CryptoTypes crypto, double quantity, OrderType orderType, OrderTypeS orderSubType, double price) { 
+        this.user = user;
+    	this.crypto = crypto;
+        this.quantity = quantity;
+        this.orderType = orderType;
+        this.orderSubType = orderSubType;
+        this.price = price;
+    }
+
+    public Order(User user, CryptoTypes crypto, double quantity, OrderType orderType, OrderTypeS orderSubType) {
+        this.user = user;
+        this.crypto = crypto;
+        this.quantity = quantity;
+        this.orderType = orderType;
+        this.orderSubType = orderSubType;
+    }
 }
